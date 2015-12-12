f5e1eb5055fbccf02b935acdde66915df8919803
Theo Styles <thus_styles@msn.com>
2015-12-12 21:43:25 +0000

    first-commit

---
+++ events.js
@@ -0,0 +1,31 @@
+function makeid(length)
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+var pusher = new Pusher('9d3ca23fe4e0cd26c73c', {
+    authEndpoint: 'localhost:3000/pusher/auth',
+    auth: {
+      headers: {
+        'X-CSRF-Token': '<%= form_authenticity_token %>'
+      }
+    }
+  });
+
+
+document.getElementById('start-session').onclick = function() {
+	document.getElementById("result").innerHTML =  "Send this to someone " + makeid(10);
+
+	channel = pusher.subscribe("private-" + id);
+
+	channel.bind('user_joined', function(data) {
+	  channel.trigger("client-website_link", { website: website });
+	});
+
+}
