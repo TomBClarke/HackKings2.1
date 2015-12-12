886ffd30863d2869a4c63def30cb1fccaded7036
rrama <ben_durrans@hotmail.co.uk>
2015-12-12 22:42:50 +0000

    ....

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
-chrome.runtime.onConnect.addListener(function(port) {
-    document.getElementsByName("body")[0].innerHTML = "";
-    port.onMessage.addListener(function(msg){
-       document.getElementsByName("body")[0].innerHTML = "";
-    });
-});
 chrome.runtime.onMessage.addListener(function(request, sender, response) {
     console.log(request.messageY);
     response({msg: "Goodbye!"});
